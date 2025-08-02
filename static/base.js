f41fe18f2b68f77ddbc7d55c3a4f0c248990aa4d
Alan Lai <alanlai22@outlook.com>
2025-08-02 11:19:03 +0800

    update on sidebar

--- static/base.js
+++ static/base.js
@@ -5,23 +5,44 @@ const sideButton = document.querySelector('#side-button');
 document.addEventListener('click', e => {
     if (sideButton.contains(e.target)) {
         if (sidebarStatus === false) {
-            document.getElementById("sidebar").style.width = "250px"
-            document.getElementById("main").style.marginLeft = "250px"
-            document.getElementById("topbar").style.marginLeft = "250px"
+            if (window.matchMedia("(max-width=575px)").matches) {
+                document.getElementById("sidebar").style.height = "200px"
+                // document.getElementById("main").style.marginTop = "250px"
+                // document.getElementById("topbar").style.marginTop = "250px"
+            }
+            else {
+                document.getElementById("sidebar").style.width = "250px"
+                document.getElementById("main").style.marginLeft = "250px"
+                document.getElementById("topbar").style.marginLeft = "250px"
+            }
             sidebarStatus = true
         }
         else {
-            document.getElementById("sidebar").style.width = "0"
-            document.getElementById("main").style.marginLeft = "0"
-            document.getElementById("topbar").style.marginLeft = "0"
+            if (window.matchMedia("(max-width=575px)".matches)) {
+                document.getElementById("sidebar").style.height = "0"
+                // document.getElementById("main").style.marginTop = "0"
+                // document.getElementById("topbar").style.marginTop = "0"
+            }
+            else {
+                document.getElementById("sidebar").style.width = "0"
+                document.getElementById("main").style.marginLeft = "0"
+                document.getElementById("topbar").style.marginLeft = "0"
+            }
             sidebarStatus = false
         }
     }
     else if (!sidebar.contains(e.target)) {
         if (sidebarStatus === true) {
-            document.getElementById("sidebar").style.width = "0"
-            document.getElementById("main").style.marginLeft = "0"
-            document.getElementById("topbar").style.marginLeft = "0"
+            if (window.matchMedia("(max-width=575px)").matches) {
+                document.getElementById("sidebar").style.height = "0"
+                // document.getElementById("main").style.marginTop = "0"
+                // document.getElementById("topbar").style.marginTop = "0"
+            }
+            else {
+                document.getElementById("sidebar").style.width = "0"
+                document.getElementById("main").style.marginLeft = "0"
+                document.getElementById("topbar").style.marginLeft = "0"
+            }
             sidebarStatus = false
         }
     }
